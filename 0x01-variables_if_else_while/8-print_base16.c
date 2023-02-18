48cf126279c86f0996c15cc135e5d9c8d2210d46
MalakHisham121 <122965150+MalakHisham121@users.noreply.github.com>
2023-02-18 04:09:07 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,15 @@
 int main(void)
 {
 	int i;
+
 	for (i = 0 ; i < 10 ; i++)
+	{
 		putchar(i + '0');
+	}
         for (j = 'a'; j <= 'f' ; j++)
+	{
 		putchar(j);
+	}
 	putchar('\n');
 	return (0);
 }
